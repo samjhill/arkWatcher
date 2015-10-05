71e09f086fbbdce335d3180a535a0b928d85b66b
Sam Hill <samuhill@gmail.com>
2015-10-04 21:53:07 -0400

    incorporate async and cmd-exec for cleanliness and callback hell freedom. Thanks guys

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 'use strict';
+var cmd = require("cmd-exec").init();
+var async = require("async");
+
 module.exports = function (str, opts){
-function cmd_exec(cmd, args, cb_stdout, cb_end) {
-  var spawn = require('child_process').spawn,
-    child = spawn(cmd, args),
-    me = this;
-  me.exit = 0;  // Send a cb to set 1 when cmd exits
-  child.stdout.on('data', function (data) { cb_stdout(me, data) });
-  child.stdout.on('end', function () { cb_end(me) });
-}
+   var calls = [];
 
-if(str == 'status'){
-var status = new cmd_exec('arkmanager', ['status'], 
-  function (me, data) {
-   if(me.stdout == undefined){
-      me.stdout = ''; 
+   if(str == 'status'){
+      console.log('getting status...');
    }
-   if( data.toString()  ) {
-      me.stdout += data.toString();}
-   },
-  function (me) {console.log(me.stdout); me.exit = 1;}
-);
-}
+   
+   calls.push(function(callback){
+      cmd.exec("arkmanager status", function(err, res){
+         if (err) {
+           return callback(err.message);
+         } else {
+	   return callback(res.message);
+         }
+     }
+  )});
 
+  async.parallel(calls, function(err, result) {
+    console.log('running');
+    if (err)
+        return console.log(err);
+    return(result);
+   });
 };
