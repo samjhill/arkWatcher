d5409293e4ba5ff07da5696b89d3542091459006
Sam Hill <samuhill@gmail.com>
2015-10-15 00:16:43 -0400

    add basic auth

--- index.js
+++ index.js
@@ -13,7 +13,31 @@ var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Add headers
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://www.samhillmade.it:8080");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  if (req.method === "OPTIONS") {
+	res.status(204);
+	res.send();
+  }
+  else {
+	next();
+  }
+ 
+});
+
 app.use(auth.connect(basic));
+
+app.post('/authenticate', function(req, res){ // if they can reach this route, they're authenticated
+    var response = {
+ 	status  : 200,
+	success : 'Updated Successfully'
+    }
+    
+    res.send(JSON.stringify(response));
+});
 
 //routes
 var publicRoutes = require('./routes/public')(app);
